05d8b744c0f30cf3066f42353a214cf1512a89d0
Vanit Kashab <vanitk1@gmail.com>
2025-10-16 15:03:05 +0100

    updates for production

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,22 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://community-events1.netlify.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 console.log('=== Environment Check ===');
